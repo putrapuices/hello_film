8a6808ebc9f35fb944ab6125e7620566d02f2d3e
putrapuices <puices14@gmail.com>
2021-12-16 15:18:43 +0700

    Setup Semantic UI ( 2 )

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Button,Icon } from 'semantic-ui-react'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   <h1>Home  </h1>
+   <Button content='Primary' primary />
+    <Button content='Secondary' secondary />
+    <Button animated>
+      <Button.Content visible>Next</Button.Content>
+      <Button.Content hidden>
+        <Icon name='arrow right' />
+      </Button.Content>
+    </Button>
+    <Button animated='vertical'>
+      <Button.Content hidden>Shop</Button.Content>
+      <Button.Content visible>
+        <Icon name='shop' />
+      </Button.Content>
+    </Button>
+    <Button animated='fade'>
+      <Button.Content visible>Sign-up for a Pro account</Button.Content>
+      <Button.Content hidden>$12.99 a month</Button.Content>
+    </Button>
     </div>
   );
 }
