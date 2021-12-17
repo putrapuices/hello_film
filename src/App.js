61b3bda03fdbcd4995c0fae866722147a1fd8b33
putrapuices <puices14@gmail.com>
2021-12-17 10:23:16 +0700

    Setup Redux ( 4 )

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import React from "react";
-import { Button,Icon } from 'semantic-ui-react'
-function App() {
-  return (
+import React,{Component} from "react";
+import { Button,Icon } from 'semantic-ui-react';
+import {connect} from 'react-redux';
+
+
+class  App  extends Component {
+  render(){
+    return(
     <div>
    <h1>Home  </h1>
-   <Button content='Primary' primary />
-    <Button content='Secondary' secondary />
-    <Button animated>
-      <Button.Content visible>Next</Button.Content>
-      <Button.Content hidden>
-        <Icon name='arrow right' />
-      </Button.Content>
-    </Button>
-    <Button animated='vertical'>
-      <Button.Content hidden>Shop</Button.Content>
-      <Button.Content visible>
-        <Icon name='shop' />
-      </Button.Content>
-    </Button>
-    <Button animated='fade'>
-      <Button.Content visible>Sign-up for a Pro account</Button.Content>
-      <Button.Content hidden>$12.99 a month</Button.Content>
-    </Button>
+    <Button content='info' info> {this.props.tes}</Button> 
     </div>
   );
 }
+}
 
-export default App;
+const stateApp = (state)=>{
+  return{
+    tes:state.tes    
+  }
+}
+export default connect(stateApp) (App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './Routes';
-import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
+import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+const stateFilm = {
+  tes: "Coba redux",
+};
 
+const reducerFilm = (state = stateFilm) => {
+  return state;
+};
+
+const store = createStore(reducerFilm);
 ReactDOM.render(
   <BrowserRouter>
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
-
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -19,5 +30,3 @@ ReactDOM.render(
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
-
-
