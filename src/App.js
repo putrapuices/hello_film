9425ea90a75ec0095f10095ccbc1c47e56e40063
putrapuices <puices14@gmail.com>
2021-12-18 09:49:26 +0700

    ada errorrnya

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React,{Component} from "react";
 import { Button,Icon } from 'semantic-ui-react';
 import {connect} from 'react-redux';
-
-
+import LoginButton from "./components/login-button";
+import LogoutButton from "./components/logout-button";
 class  App  extends Component {
   render(){
     return(
     <div>
+      <LoginButton/>
+      <LogoutButton/>
    <h1>Home  </h1>
     <Button content='info' info> {this.props.tes}</Button> 
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
@@ -17,9 +18,15 @@ const reducerFilm = (state = stateFilm) => {
 const store = createStore(reducerFilm);
 ReactDOM.render(
   <BrowserRouter>
+  <Auth0Provider
+    domain="puices.us.auth0.com"
+    clientId="cCqRCdsrvl7DnxLC9tIJPMhRESh3BxzT"
+    redirectUri={window.location.origin}
+  >
     <Provider store={store}>
       <Routes />
     </Provider>
+  </Auth0Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
